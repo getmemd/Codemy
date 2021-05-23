043d0fa13819110f8c84f3b1531104f9c41efe38
Adilkhan <amedeuyev@gmail.com>
2021-05-24 00:18:49 +0600

    Initial Commit

---
+++ CodemyProject/CodemyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodemyProjectApp.swift
+//  CodemyProject
+//
+//  Created by Адиль Медеуев on 24.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CodemyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
